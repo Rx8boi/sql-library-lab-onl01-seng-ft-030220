dfb9dd661d93280a459ee805cdad75eec07c3461
Charles Butler <cdbutle@outlook.com>
2020-04-10 20:42:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(SPECIES) FROM characters SORT species ASC "
+  "SELECT species, COUNT(SPECIES) FROM characters ORDER BY species ASC GROUP BY species "
 end
 
 def select_name_and_series_subgenres_of_authors
