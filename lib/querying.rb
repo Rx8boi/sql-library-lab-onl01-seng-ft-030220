ff0035defabdf9964a516e6fbec432f2e7de9ac9
Charles Butler <cdbutle@outlook.com>
2020-04-10 21:04:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,9 +14,9 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT author.name, subgenres.name FROM series
   INNER JOIN authors
-  WHERE series.author_id = authors.id
-  INNER JOIN subgenres
-  WHERE subgenres.id = series.subgenre_id"
+  ON series.author_id = authors.id
+  INNER JOIN subgenres 
+  ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
