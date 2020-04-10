d9682413ae86dc1cd5f8e9609494e8ff821f5b9d
Charles Butler <cdbutle@outlook.com>
2020-04-10 19:54:07 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE authors (id INTEGER
 PRIMARY KEY, name TEXT);
 
 CREATE TABLE books (id INTEGER
-PRIMARY KEY, title TEXT, year INTEGER);
+PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
 
 CREATE TABLE characters (id INTEGER
 PRIMARY KEY,name TEXT, motto TEXT, species TEXT, author_id INTEGER);
