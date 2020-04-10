c119268672684f57faa06b963d8710a7456c1da5
Charles Butler <cdbutle@outlook.com>
2020-04-10 21:26:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM character_books
+  INNER JOIN books
+  ON character_books.book_id = books.id
+  INNER JOIN series
+  ON series.id = books.series_id
+  ORDER BY species ASC"
 end
 
 def select_character_names_and_number_of_books_they_are_in
